a5497533058ec3100f98c5b51f5116199fb29e45
Alexandre Lopes <alopes@sfu.ca>
2020-06-13 00:37:37 -0700

    Fix on python sample environment

--- postgres/pythonsample_pg.sh
+++ postgres/pythonsample_pg.sh
@@ -4,6 +4,6 @@
 #SBATCH --output=%x-%j.out
 
 export LD_LIBRARY_PATH=/cvmfs/soft.computecanada.ca/nix/var/nix/profiles/postgresql-10.2/lib
-source ~/python/bin/activate
+source venv/bin/activate
 python sample_pg.py > sample_pg.rep
 
